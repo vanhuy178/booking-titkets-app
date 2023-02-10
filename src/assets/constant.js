ad2b4b79890e0b11bd2c56a2c4ce07fa75372409
HUY_FA <nguyenvanhuy178tiengiang@gmail.com>
2023-02-10 10:46:13 +0700

    starting my project 1.0.0

---
+++ src/assets/constant.js
@@ -0,0 +1,54 @@
+export const data = [
+    {
+        maMonAn: 'MA01',
+        tenMonAn: 'Cơm chiên dương châu',
+        gia: 200,
+        hinhAnh: 'http://casestudy.cyberlearn.vn/img/MA01.jpeg',
+        maDanhMuc: 'MC',
+        tenDanhMuc: 'Món chiên'
+    },
+    {
+        maMonAn: 'MA02',
+        tenMonAn: 'Cơm chiên cá mặn',
+        gia: 300,
+        hinhAnh: 'http://casestudy.cyberlearn.vn/img/MA02.jpeg',
+        maDanhMuc: 'MC',
+        tenDanhMuc: 'Món chiên'
+    },
+    {
+        maMonAn: 'MA03',
+        tenMonAn: 'Gà nướng muối ớt',
+        gia: 500,
+        hinhAnh: 'http://casestudy.cyberlearn.vn/img/MA03.jpeg',
+        maDanhMuc: 'MN',
+        tenDanhMuc: 'Món nướng'
+    },
+    {
+        maMonAn: 'MA04',
+        tenMonAn: 'Gà nướng muối tiêu chanh',
+        gia: 600,
+        hinhAnh: 'http://casestudy.cyberlearn.vn/img/MA04.jpeg',
+        maDanhMuc: 'MN',
+        tenDanhMuc: 'Món nướng'
+
+
+    },
+    {
+        maMonAn: 'MA05',
+        tenMonAn: 'Trà đào cam sả',
+        gia: 50,
+        hinhAnh: 'http://casestudy.cyberlearn.vn/img/MA05.jpeg',
+        maDanhMuc: 'GK',
+        tenDanhMuc: 'Giải khát',
+    },
+    {
+        maMonAn: 'MA06',
+        tenMonAn: 'Bia heniken',
+        gia: 50,
+        hinhAnh: 'http://casestudy.cyberlearn.vn/img/MA06.jpeg',
+        maDanhMuc: 'GK',
+        tenDanhMuc: 'Giải khát',
+    },
+]
+
+
